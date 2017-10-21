b97128e096b6c4fe2d102b086e17648cddfc7c1a
tmy5277 <1223688937@qq.com>
2017-10-21 20:46:25 +0800

    header组件开发二 与 star组件开发

---
+++ src/components/star/star.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="star" :class="starType">
+		<span v-for="itemClass in itemClasses" 
+		:class="itemClass" class="star-item"></span>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			size: {
+				type: Number
+			},
+			score: {
+				type: Number
+			}
+		},
+		computed: {
+			starType(){
+				return 'star-' + this.size;
+			}
+		},
+	};
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+	.star
+		font-size: 0
+		.star-item
+			display: inline-block
+			background-repeat: no-repeat
+		&.star-48
+		&.star-36
+		&.star-24
+		
+			
+</style>
